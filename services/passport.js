766c190c0648b7ea54b162dd85f019d198b70a44
Matthew Butt <matthew.y.butt@gmail.com>
2017-08-23 14:41:41 -0700

    add cookie-session and login logic for mongo

--- services/passport.js
+++ services/passport.js
@@ -1,16 +1,40 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users')
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, id)
+    });
+});
+
 passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  },
-  (accessToken, refreshToken, profile, done) => {
-    console.log('accessToken: ', accessToken)
-    console.log('refreshToken: ', refreshToken)
-    console.log('profile: ', profile)
-  })
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({googleId: profile.id})
+        .then((existingUser) => {
+          if (existingUser) {
+            done(null, existingUser);//this is a Passport function
+          } else {
+            new User({googleId: profile.id})
+              .save()
+              .then(user => done(null, user));
+          }
+        }
+      )
+    }
+  )
 );
