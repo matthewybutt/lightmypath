b0c021049138cb72d6642f24a7ab31f2d93eb448
Matthew Butt <matthew.y.butt@gmail.com>
2017-08-24 16:16:58 -0700

    install and initial set up of react, router and redux

--- services/passport.js
+++ services/passport.js
@@ -24,18 +24,13 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({googleId: profile.id})
-        .then((existingUser) => {
-          if (existingUser) {
-            done(null, existingUser);//this is a Passport function
-          } else {
-            new User({googleId: profile.id})
-              .save()
-              .then(user => done(null, user));
-          }
-        }
-      )
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({googleId: profile.id})
+      if (existingUser) {
+        return done(null, existingUser);//this is a Passport function
+      }
+      const user = await new User({googleId: profile.id}).save()
+      done(null, user)
     }
   )
 );
