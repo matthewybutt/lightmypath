e5c3199c4583307730ca9d0ba3993671f0a61e39
Matthew Butt <matthew.y.butt@gmail.com>
2017-08-24 17:17:06 -0700

    set up router, import materialize

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
+import 'materialize-css/dist/css/materialize.min.css'
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App'
+import reducers from './reducers'
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDom.render(
   <Provider store={store}>
