bf94c015e462cfc39973cedab5d479ded73c2cde
Matthew Butt <matthew.y.butt@gmail.com>
2017-10-08 13:01:03 -0700

    add login check to landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Landing = () => {
-  return(
-    <div style={{textAlign: 'center'}}>
-      <h1 style={{marginBottom: "0"}}>LMP</h1>
-      <p style={{marginTop: "0"}}>Light | My | Path</p>
-      <p>Memorize verses today!</p>
-      <div className="row">
-        <div className="col s6" style={{textAlign:"right"}}>
-          <Link to={"/verse/new"}><button className="btn waves-effect waves-light red darken-4">New Verse</button></Link>
-        </div>
-        <div className="col s6" style={{textAlign:"left"}}>
-          <Link to={"/verse/my_verses"}><button className="btn waves-effect waves-light red darken-4">My Verses</button></Link>
-        </div>
+class Landing extends Component {
+
+  render(){
+    return(
+      <div style={{textAlign: 'center'}}>
+        <h1 style={{marginBottom: "0"}}>LMP</h1>
+        <p style={{marginTop: "0"}}>Light | My | Path</p>
+        <p>Memorize verses today!</p>
+        {this.props.auth ?
+          <div className="row">
+            <div className="col s6" style={{textAlign:"right"}}>
+              <Link to={"/verse/new"}><button className="btn waves-effect waves-light red darken-4">New Verse</button></Link>
+            </div>
+            <div className="col s6" style={{textAlign:"left"}}>
+              <Link to={"/verse/my_verses"}><button className="btn waves-effect waves-light red darken-4">My Verses</button></Link>
+            </div>
+          </div>
+          :
+          <a href="/auth/google"><button className="btn waves-effect waves-light red darken-4">Log In</button></a>
+        }
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default Landing;
+function mapStateToProps({auth}) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Landing);
